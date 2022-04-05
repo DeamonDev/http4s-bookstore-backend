ed32a3d8cffe679f829d21fa99dcbc79052ec3ec
deamondev <deamondev@gmail.com>
2022-04-05 13:29:07 +0200

    second

--- build.sbt
+++ build.sbt
@@ -8,7 +8,8 @@ ThisBuild / organizationName := "example"
 lazy val root = (project in file("."))
   .settings(
     name := "book-store",
-    libraryDependencies += scalaTest % Test
+    libraryDependencies += scalaTest % Test,
+    libraryDependencies ++= http4s
   )
 
 // See https://www.scala-sbt.org/1.x/docs/Using-Sonatype.html for instructions on how to publish to Sonatype.
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,5 +1,18 @@
 import sbt._
 
 object Dependencies {
+
+  object Versions {
+    val http4sVersion: String = "1.0.0-M23"
+  }
+
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.11"
+
+  import Versions._
+
+  lazy val http4s = Seq(
+    "org.http4s" %% "http4s-dsl" % http4sVersion,
+    "org.http4s" %% "http4s-ember-server" % http4sVersion,
+    "org.http4s" %% "http4s-ember-client" % http4sVersion
+  )
 }
